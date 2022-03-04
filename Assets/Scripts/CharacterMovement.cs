a284bef5fb63cbe7b7ecca8395c33f61f4571644
Scott Norwood <weasddddd@gmail.com>
2022-03-03 20:47:52 -0600

    Initial commit for Jack.

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Scott.Norwood
+{
+    public class CharacterMovement : MonoBehaviour
+    {
+        public Controller controller;
+        public float movementSpeed = 40f;
+        float horizontalMovement = 0f;
+        bool jump = false;
+
+        // Update is called once per frame
+        void Update()
+        {
+            horizontalMovement = Input.GetAxisRaw("Horizontal") * movementSpeed;
+            if (Input.GetButtonDown("Jump"))
+            {
+                jump = true;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            controller.Move(horizontalMovement * Time.fixedDeltaTime, false, jump);
+            jump = false;
+        }
+    }
+}
